c79a7c635c6f5a66954719ba94043325f8615ded
hoangnh17 <hoangnh17@hdbank.com.vn>
2025-11-18 18:12:29 +0700

    feat: add config database

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,15 @@
-package api
+package main
+
+import (
+	"book-manager/pkg/database"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		return
+	}
+	database.Connect()
+}
